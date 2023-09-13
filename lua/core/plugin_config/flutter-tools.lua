ea8957a015aff055329106d50a8dabc078cafdaa
sangyeon kim <greyhairchooselife@gmail.com>
2023-09-13 20:47:53 +0900

    flutter-tools 부가기능 자동실행

--- lua/core/plugin_config/flutter-tools.lua
+++ lua/core/plugin_config/flutter-tools.lua
@@ -2,13 +2,16 @@ require("flutter-tools").setup {
   widget_guides = {
     enabled = true,
   },
+  outline = {
+    auto_open = true,
+  },
   closing_tags = {
     highlight = "ErrorMsg",
     prefix = "▾ ",
     enabled = false,
   },
   dev_log = {
-    enabled = false,
+    enabled = true,
     notify_errors = false,
     open_cmd = "tabedit",
   },
