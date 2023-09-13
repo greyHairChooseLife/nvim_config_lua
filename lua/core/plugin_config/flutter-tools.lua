2850b2f8d00f11a2b880c0ffed62a545138cc626
sangyeon kim <greyhairchooselife@gmail.com>
2023-09-13 21:23:52 +0900

    ouline autorun옵션은 패널 사이즈 이슈가 있다. 내가 사이즈 조절 못하는 문제일수도 있지만...

--- lua/core/plugin_config/flutter-tools.lua
+++ lua/core/plugin_config/flutter-tools.lua
@@ -2,9 +2,10 @@ require("flutter-tools").setup {
   widget_guides = {
     enabled = true,
   },
-  outline = {
-    auto_open = true,
-  },
+-- it restricts a size of the outline like shit
+--  outline = {
+--    auto_open = false, 
+--  },
   closing_tags = {
     highlight = "ErrorMsg",
     prefix = "▾ ",
@@ -16,3 +17,21 @@ require("flutter-tools").setup {
     open_cmd = "tabedit",
   },
 } -- use defaults
+
+function toggleFlutterToolsOutline() vim.cmd('FlutterOutlineToggle') end
+vim.cmd('command! FTouTline lua toggleFlutterToolsOutline()')
+
+function openFlutterEmulator() vim.cmd('FlutterEmulators') end
+vim.cmd('command! FTemuLator lua openFlutterEmulator()')
+
+function runFlutter() vim.cmd('FlutterRun') end
+vim.cmd('command! FTruN lua runFlutter()')
+
+function restartFlutter() vim.cmd('FlutterRestart') end
+vim.cmd('command! FTresTart lua restartFlutter()')
+
+function openFlutterDevTools() vim.cmd('FlutterOpenDevTools') end
+vim.cmd('command! FTdeVtool lua openFlutterDevTools()')
+
+function openVisualDebug() vim.cmd('FlutterVisualDebug') end
+vim.cmd('command! FTvisualDebug lua openVisualDebug()')
