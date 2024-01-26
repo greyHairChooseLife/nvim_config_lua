7687d97e1d1b558b49b5065637726f5311ec3244
sangyeon kim <greyhairchooselife@gmail.com>
2024-01-26 18:30:54 +0900

    add keymaps for telescope

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -15,8 +15,12 @@ vim.keymap.set('n', '<leader><leader>tq', ':tabclose!<CR>') --
 vim.keymap.set('n', '<leader><leader>d', ':NvimTreeFindFileToggle<CR>') -- paste last thing yanked, not deleted
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fft', builtin.filetypes, {})
+vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fc', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>fm', builtin.marks, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fs', builtin.git_stash, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 vim.keymap.set('n', '<leader><leader>C', ':Calendar<CR>')
