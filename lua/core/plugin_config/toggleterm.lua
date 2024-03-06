2c0b417c6593f0d62da59a1b409d767cc594945d
sangyeon kim <greyhairchooselife@gmail.com>
2024-03-06 20:23:52 +0900

    fix toggleterm size

--- lua/core/plugin_config/toggleterm.lua
+++ lua/core/plugin_config/toggleterm.lua
@@ -1,11 +1,11 @@
 require("toggleterm").setup{
-  size = vim.o.columns * 0.4,
+  size = 100,
   open_mapping = [[<c-\>]],
   hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_filetypes = {},
   start_in_insert = true,
   insert_mappings = true, -- whether or not the open mapping applies in insert mode
-  persist_size = true,
+  persist_size = false,
   direction = 'vertical',
   close_on_exit = true, -- close the terminal window when the process exits
   shell = vim.o.shell, -- change the default shell
@@ -18,5 +18,11 @@ require("toggleterm").setup{
       border = "Normal",
       background = "Normal",
     }
-  }
+  },
+  --  This field is only relevant if direction is set to 'float'
+--  highlights = {
+--    Normal = {
+--      guibg = "TabLine",
+--    }
+--  }
 }
