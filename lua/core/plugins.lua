d5cf7d15b376f2affff5486fd3c37c0cd6c449a2
sangyeon kim <greyhairchooselife@gmail.com>
2024-03-10 16:42:24 +0900

    remove more

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -37,10 +37,6 @@ return require('packer').startup(function(use)
   }
   use 'sindrets/winshift.nvim'
 
-  use {"akinsho/toggleterm.nvim", tag = '*', config = function()
-    require("toggleterm").setup()
-  end}
-
 	-- Automatically set up your configuration after cloning packer.nvim   
 	-- Put this at the end after all plugins 
 	if packer_bootstrap then     
