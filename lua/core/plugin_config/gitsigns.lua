c3c422c513f60e3607a38582337d9ec49aba4538
sangyeon kim <greyhairchooselife@gmail.com>
2024-06-24 03:57:31 +0900

    color for gitsigns

--- lua/core/plugin_config/gitsigns.lua
+++ lua/core/plugin_config/gitsigns.lua
@@ -16,11 +16,12 @@ require('gitsigns').setup({
   },
   signs_staged_enable = true,
   signs_staged = {
-    add          = { text = '✔' },
-    change       = { text = '✔' },
-    delete       = { text = '✔' },
-    topdelete    = { text = '✔' },
-    changedelete = { text = '✔' },
+    add          = { hl = 'GitSignsAdd', text = '✔' },
+    change       = { hl = 'GitSignsChange', text = '✔' },
+    delete       = { hl = 'GitSignsChangedelete', text = '✔' },
+    topdelete    = { hl = 'GitSignsDelete', text = '✔' },
+    changedelete = { hl = 'GitSignsTopdelete', text = '✔' },
+    untracked    = { hl = 'GitSignsUntracked', text = '✔' }
   },
 })
 
