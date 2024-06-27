1b167605118d126759998e08a1918b302a1699e9
sangyeon kim <greyhairchooselife@gmail.com>
2024-06-27 17:48:47 +0900

    plugin: nvim-notify

---
+++ lua/core/plugin_config/nvim-notify.lua
@@ -0,0 +1,23 @@
+require("notify").setup {
+  background_colour = "NotifyBackground",
+  fps = 200,
+  icons = {
+    DEBUG = "",
+    ERROR = "",
+    INFO = "",
+    TRACE = "✎",
+    WARN = ""
+  },
+  level = 2,
+  minimum_width = 50,
+  render = "default",
+  stages = "fade_in_slide_out",
+  time_formats = {
+    notification = "%H:%M",
+    notification_history = "%FT%T"
+  },
+  timeout = 2000,
+  top_down = true
+}
+
+Notify = require('notify')
