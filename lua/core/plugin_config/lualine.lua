a7eb4ad3ddd1dc6575169cb4c243af30d288296d
sangyeon kim <greyhairchooselife@gmail.com>
2024-08-04 14:47:00 +0900

    hide lualine while {'packer', 'NvimTree', 'alpha'}

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -39,7 +39,7 @@ require('lualine').setup {
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
     disabled_filetypes = {
-      statusline = {},
+      statusline = { 'packer', 'NvimTree', 'alpha' },
       winbar = {},
     },
     ignore_focus = {},
