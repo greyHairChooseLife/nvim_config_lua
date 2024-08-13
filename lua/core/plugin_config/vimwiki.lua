0f5928e850fbe282fa233333421d0ba10fc86b6c
sangyeon kim <greyhairchooselife@gmail.com>
2024-08-13 23:51:35 +0900

    keymap:

--- lua/core/plugin_config/vimwiki.lua
+++ lua/core/plugin_config/vimwiki.lua
@@ -26,6 +26,10 @@ vim.g.vimwiki_list = {
   }
 }
 
+-- TODO:
+-- "Vimwiki Shift+Return, Ctrl+Return not working."
+-- https://www.reddit.com/r/vim/comments/7xoe8b/vimwiki_shiftreturn_ctrlreturn_not_working/
+
 -- NOTE: markdown.nvim의 gui가 좋아서 딱히 필요 없을 듯
 -- -- Set tabstop to 4 for vimwiki files
 -- vim.cmd([[
@@ -36,3 +40,32 @@ vim.g.vimwiki_list = {
 --     autocmd BufRead,BufNewFile ~/Documents/job-wiki/notes/*.md setlocal tabstop=4 shiftwidth=4 expandtab
 --   augroup END
 -- ]])
+
+-- NOTE:: ALTERNATIVE LIST
+-- ..
+-- https://www.reddit.com/r/neovim/comments/10ulqji/note_taking_options/
+-- I cant recommend any specific plugin but I can give you a list of available note/wiki/org-plugins:
+-- *anki
+-- *arachne
+-- *dotoo
+-- *flashcards
+-- *kb-notes
+-- *linny
+-- *mind
+-- *nabla
+-- *neorg
+-- *neuron.nvim
+-- *neuron.vim
+-- *obsidian
+-- *orgmode.nvim
+-- *orgmode
+-- *orgwiki
+-- *riv
+-- *samwise
+-- *scribe
+-- *todotxt
+-- *vimwiki
+-- *wiki
+-- *YANP
+-- *zettelkasten
+-- *zk
