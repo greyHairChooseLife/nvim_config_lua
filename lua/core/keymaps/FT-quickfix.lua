f1552ad6efe28e4d1cbf1a0c6be659aa124e28ed
sangyeon kim <greyhairchooselife@gmail.com>
2024-08-15 22:57:15 +0900

    refactor:

--- lua/core/keymaps/FT-quickfix.lua
+++ lua/core/keymaps/FT-quickfix.lua
@@ -2,15 +2,8 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "qf",
   callback = function()
     vim.keymap.set('n', 'dd', QF_RemoveItem, { buffer = true, silent = true })
+    vim.keymap.set('n', 'DF', QF_ClearList, { buffer = true, silent = true })
+    vim.keymap.set('n', '<C-n>', QF_MoveNext, { buffer = true })
+    vim.keymap.set('n', '<C-p>', QF_MovePrev, { buffer = true })
   end
 })
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "qf",
-  callback = function()
-    vim.keymap.set('n', 'DD', QF_ClearList, { buffer = true, silent = true })
-  end
-})
-
-vim.keymap.set('n', '<C-n>', QF_MoveNext, { buffer = true })
-vim.keymap.set('n', '<C-p>', QF_MovePrev, { buffer = true })
