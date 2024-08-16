c692405726411dfc287f99d14c8c5f6293d9ee7c
sangyeon kim <greyhairchooselife@gmail.com>
2024-08-16 20:52:18 +0900

    typo:

--- lua/core/keymaps/FT-quickfix.lua
+++ lua/core/keymaps/FT-quickfix.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "qf",
   callback = function()
     vim.keymap.set('n', 'dd', QF_RemoveItem, { buffer = true, silent = true })
-    vim.keymap.set('n', 'DF', QF_ClearList, { buffer = true, silent = true })
+    vim.keymap.set('n', 'DD', QF_ClearList, { buffer = true, silent = true })
     vim.keymap.set('n', '<C-n>', QF_MoveNext, { buffer = true })
     vim.keymap.set('n', '<C-p>', QF_MovePrev, { buffer = true })
   end
