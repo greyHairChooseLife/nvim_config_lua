c214067ab3f5600096b037f61101d2059067dfc6
sangyeon kim <greyhairchooselife@gmail.com>
2024-09-20 18:24:12 +0900

    todo:

--- init.lua
+++ init.lua
@@ -10,3 +10,5 @@ require("core.temp");
 
 -- https://www.lua.org/manual/5.3/
 -- https://neovim.io/doc/user/options.html
+-- TODO:
+-- nvim tree에서 현재 열려있는 버퍼에 해당하는 파일 및 폴더를 표시해주면 좋겠다. 그러면 telescope으로 조회하고 선택하지 않아도 되니, 그런 경우에 도움이 될듯?
