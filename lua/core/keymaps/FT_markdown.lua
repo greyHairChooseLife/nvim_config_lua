6c2179ec32c987ad93446ef2796c7a5dd86451cb
sangyeon kim <greyhairchooselife@gmail.com>
2024-09-21 19:30:13 +0900

    UI: markdown에서는 BLANKLINE: On (as a deafult)

--- lua/core/keymaps/FT_markdown.lua
+++ lua/core/keymaps/FT_markdown.lua
@@ -1,6 +1,10 @@
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
+    -- GUI
+    vim.cmd('IBLToggle')
+
+    -- keymaps
     vim.keymap.set('n', 'PP', '<cmd>MarkdownPreview<CR>', { buffer = true })
   end,
 })
