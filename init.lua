ae42b91bba9e17b1ab341517da35326c1eef0e1e
sangyeon kim <greyhairchooselife@gmail.com>
2024-10-04 17:53:21 +0900

    refactor: custom tabline을 별도 파일로 분리

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ require("core.auto_commands");
 require("core.snippets");
 require("core.keymaps");
 require("core.highlights");
+require("core.custom_tabline");
 require("core.temp");
 
 -- https://www.lua.org/manual/5.3/
