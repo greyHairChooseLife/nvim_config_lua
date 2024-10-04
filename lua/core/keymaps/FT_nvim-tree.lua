0d8281e3945b92c458f5b467186ed56cdadd394d
sangyeon kim <greyhairchooselife@gmail.com>
2024-10-04 16:06:12 +0900

    feat: nvim-tree에서 oil 열기, '-', '_'

--- lua/core/keymaps/FT_nvim-tree.lua
+++ lua/core/keymaps/FT_nvim-tree.lua
@@ -6,17 +6,50 @@ vim.api.nvim_create_autocmd("FileType", {
 
     -- gui
 
-    -- keymap
-    vim.keymap.set({ 'n', 'v' }, 'gq', function()
-      api.tree.close_in_this_tab()
-    end , { buffer = true })
+    -- local function
+    local function open_oil_relatively(mode) -- oil 켜기
+      -- nvim-tree API를 이용해 현재 커서 위치의 노드 가져오기
+      local node = require('nvim-tree.api').tree.get_node_under_cursor()
 
-    vim.keymap.set({ 'n', 'v' }, '-', function()
-      vim.cmd('tabnew')
-      local tabnr = vim.fn.tabpagenr()
-      vim.fn.settabvar(tabnr, 'tabname', 'Oil')
-      require('oil').open()
-    end, { buffer = true })
+      if node then
+        local path
+
+        if node.type == "file" then
+          -- 파일인 경우 부모 디렉토리 경로 가져오기
+          path = vim.fn.fnamemodify(node.absolute_path, ":h")
+        elseif node.type == "directory" then
+          -- 디렉토리인 경우 해당 디렉토리 경로 사용
+          path = node.absolute_path
+        end
+
+        -- 경로가 nil인지 확인하여 최상위 경로 처리
+        if path and (path == "/" or path == vim.fn.fnamemodify(path, ":h")) then
+          path = node.absolute_path
+        end
+
+        if mode == "tab" then
+          vim.cmd('tabnew')
+          local tabnr = vim.fn.tabpagenr()
+          vim.fn.settabvar(tabnr, 'tabname', 'Oil')
+        else
+          vim.cmd('vsplit')
+        end
+
+        -- path가 유효한지 확인 후 작업 수행
+        if path then
+          require('oil').open(path)  -- 필요한 경우 이 부분을 활용
+        else
+          require('oil').open()
+        end
+      else
+        vim.notify('No node selected in nvim-tree.')
+      end
+    end
+
+    -- keymap
+    vim.keymap.set({ 'n', 'v' }, '-', function() open_oil_relatively() end, { noremap = true, silent = true })
+    vim.keymap.set({ 'n', 'v' }, '_', function() open_oil_relatively("tab") end, { noremap = true, silent = true })
+
   end
 })
 
