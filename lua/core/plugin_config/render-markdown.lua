40345ca5c03d79ff5bfa0d928063be5166e99ccf
sangyeon kim <greyhairchooselife@gmail.com>
2024-10-31 18:44:19 +0900

    remove: some comment

--- lua/core/plugin_config/render-markdown.lua
+++ lua/core/plugin_config/render-markdown.lua
@@ -205,7 +205,7 @@ require('render-markdown').setup({
     --  󰓛 󰨔 󰄱    
     -- icons = { '●', '○', '◆', '◇', '▪', '•', '-', '▫' },
     icons = { '●', '○', '◆', '◇', '▪', '-' },
-    -- ordered_icons = {{'1', '2', '3', '4'}, {'a', 'b', 'c'},}, -- TODO:: 아마도 list of lists 형태로 업데이트 될듯, 기다렸다가 사용하자.
+    -- ordered_icons = {{'1', '2', '3', '4'}, {'a', 'b', 'c'},},
     -- Padding to add to the right of bullet point
     right_pad = 0,
     -- Highlight for the bullet icon
