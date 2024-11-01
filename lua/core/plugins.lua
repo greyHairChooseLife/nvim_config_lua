5d5bd96918a3602481e9328fb8679c3f7786264b
sangyeon kim <greyhairchooselife@gmail.com>
2024-11-02 02:45:53 +0900

    feat: comment.nvim에서 tsx, jsx, html등도 지원하도록 개선

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -115,9 +115,22 @@ return require('packer').startup(function(use)
   use 'stevearc/aerial.nvim'
 
   -- https://github.com/numToStr/Comment.nvim?tab=readme-ov-file
-  use { 'numToStr/Comment.nvim', config = function()
-    require('Comment').setup()
-  end }
+  use {
+    'numToStr/Comment.nvim',
+    requires = { 'JoosepAlviste/nvim-ts-context-commentstring' },
+    config = function()
+      -- import comment plugin safely
+      local comment = require("Comment")
+
+      local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+      -- enable comment
+      comment.setup({
+        -- for commenting tsx, jsx, svelte, html files
+        pre_hook = ts_context_commentstring.create_pre_hook(),
+      })
+    end
+  }
   --use {'JoosepAlviste/nvim-ts-context-commentstring'}
   --block comment 달면 되는데 꼭 설치해야할까? 일단 미루자.
 
