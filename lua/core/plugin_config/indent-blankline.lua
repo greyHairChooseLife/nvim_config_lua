2112f82f88e9e4965aa52dc20eee589d2bbff322
sangyeon kim <greyhairchooselife@gmail.com>
2024-11-02 03:53:20 +0900

    UI:

--- lua/core/plugin_config/indent-blankline.lua
+++ lua/core/plugin_config/indent-blankline.lua
@@ -1,20 +1,23 @@
+vim.api.nvim_set_hl(0, "MyBG", { fg = "#24283b" })
+
 require("ibl").setup {
   enabled = false,
   indent = {
     char = "▏",
     smart_indent_cap = true,
     repeat_linebreak = false,
-
+    highlight = { "MyBG" },
   },
   -- whitespace = { highlight = { "Whitespace", "NonText" } },
   scope = {
     enabled = true,
-    char = "▍",
-    highlight = { "DevIconKotlin" },
-    show_start = true,
-    show_end = true,
+    -- char = "▍",
+    char = "▏",
+    highlight = { "NvimTreeIndentMarker" },
+    show_start = false,
+    show_end = false,
     injected_languages = false,
     priority = 500,
-    exclude = { language = { "lua" } },
+    -- exclude = { language = { "lua" } },
   },
 }
