b8f7fcc3cadc71dce2ff812f43ab7c5a8ee559d4
sangyeon kim <greyhairchooselife@gmail.com>
2024-11-12 22:03:46 +0900

    keymap: avante quit

--- lua/core/keymaps/FT_avante.lua
+++ lua/core/keymaps/FT_avante.lua
@@ -1,3 +1,5 @@
+local opts = { noremap = true, silent = true, buffer = true }
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "Avante",
   callback = function()
@@ -5,15 +7,22 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.api.nvim_set_hl(0, "AvanteBufferHighlight", { bg = "#242024" })
     vim.api.nvim_set_hl(0, "AvanteBufferEOB", { fg = "#242024" })
     vim.cmd("setlocal winhighlight=Normal:AvanteBufferHighlight,SignColumn:AvanteBufferHighlight,EndOfBuffer:AvanteBufferEOB")
+
+    -- KEYMAP
+    vim.keymap.set({ 'n', 'v' }, 'gq', '<cmd>AvanteToggle<cr>', opts)
   end
+
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "AvanteInpu",
+  pattern = "AvanteInput",
   callback = function()
     -- GUI
     -- vim.api.nvim_set_hl(0, "FugitiveBufferHighlight", { bg = "#242024" })
     -- vim.api.nvim_set_hl(0, "FugitiveBufferEOB", { fg = "#242024" })
     -- vim.cmd("setlocal winhighlight=Normal:FugitiveBufferHighlight,SignColumn:FugitiveBufferHighlight,EndOfBuffer:FugitiveBufferEOB")
+
+    -- KEYMAP
+    vim.keymap.set({ 'n', 'v' }, 'gq', '<cmd>AvanteToggle<cr>', opts)
   end
 })
