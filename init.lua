63b08792cb17671b23f025d59ddd5b28708a7fde
sangyeon kim <greyhairchooselife@gmail.com>
2024-11-13 06:46:50 +0900

    todo:

--- init.lua
+++ init.lua
@@ -65,3 +65,17 @@ require("core.floating_window");
 -- indent-blankline의 current_scope_only 옵션이 부재한 것을 보완히기 위해 사용할수도 있을듯. <Space>에 추가 맵핑하자.
 -- indent-blankline의 경우 lv.1 line은 무시하는 것도 좋겠다. -> https://github.com/lukas-reineke/indent-blankline.nvim/issues/824
 -- 별개로 다른 indent-line 플러그인도 고려해보자. mini같ㅇ너거
+
+-- TODO:
+-- nvim-tree와 버퍼 관리 키맵, 특히 'gq'에 맵핑된 것이 제대로 동작을 안하는 부분이 있다. 문제되는 부분이 명확히 정리되지도 않았다.
+-- 이와 관련하여 nvim-tree의 wiki에 내용이 있다. nvim-tree 버퍼 외 1개 버퍼만 있을 시 bq로 윈도우가 완전히 닫히지 않는 문제다.
+-- (더불어 해당 wiki에 좋은 내용이 많으니 다시 한번 훑어보며 가져올만한게 없나 살피자.)
+
+-- TODO:
+-- fugitive 'I'에 맵핑한 git add -p 의 기능이 뭔가 이상하다. esc로 빠져나가는 부분이 정상이 아닌듯. 버퍼 안에 버퍼를 띄우는데, 혹시 flatten.nvim과 관련 있는 내용이지 싶다.
+
+-- TODO:
+-- 전반적으로 더 읽기쉽게 개선해볼 수 있을듯..
+
+-- TODO:
+-- lualine 반응이 매우 느려졌다. 왜? 개선이 필요하다.
