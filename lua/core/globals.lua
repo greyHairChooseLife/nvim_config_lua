0bea48d1884ff71f673332757faf6a6e7cde7ebc
sangyeon kim <greyhairchooselife@gmail.com>
2024-11-20 07:39:48 +0900

    etc: 스타일 가이드

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -1,40 +1,42 @@
-vim.opt.clipboard = "unnamedplus"
-vim.opt.number = true;
-vim.opt.relativenumber = true;
+local opt = vim.opt
+local g = vim.g
+local diag = vim.diagnostic
 
-vim.opt.expandtab = true;
-vim.opt.autoindent = true;
-vim.opt.swapfile = false;
+-- General editor settings
+opt.clipboard = "unnamedplus"
+opt.mouse = ""
+opt.undofile = true -- Maintain undo history between sessions
+opt.swapfile = false
 
-vim.opt.tabstop = 2;
-vim.opt.shiftwidth = 2;
+-- UI settings
+opt.number = true
+opt.relativenumber = true
+opt.fillchars = 'vert:┃'
+opt.splitright = true -- Open vertical splits to the right
 
-vim.diagnostic.config({
+-- Indentation settings
+opt.expandtab = true
+opt.autoindent = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+
+-- Folding settings
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevel = 99
+opt.foldmethod = "indent"
+opt.foldenable = false
+opt.formatoptions:remove('f') -- Prevent auto-folding during formatting
+
+-- Session settings
+opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
+
+-- Global variables
+g.mapleader = "\\" -- Set leader key
+
+-- Diagnostic settings
+diag.config({
   underline = true,
   virtual_text = false,
   signs = true,
   update_in_insert = false,
 })
-
--- vim.opt.foldmethod = "manual"
---vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldlevel = 99
-
-vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
-
-vim.opt.foldmethod = "indent"
-vim.opt.foldenable = false
-vim.opt.formatoptions:remove('f') -- 코드 작성 시 폴드가 갑자기 접히는 문제가 있다면 formatoptions의 fold 플래그 때문일 수 있습니다. 이 플래그가 설정되어 있으면 포매팅 중에 폴드를 자동으로 다시 접을 수 있습니다.
-
-vim.g.mapleader = "\\" -- <leader> as Space
-
--- Set fillchars
-vim.o.fillchars = 'vert:┃'
-
-vim.opt.splitright = true -- 수직 분할시 오른쪽에 창 생성
--- vim.opt.splitbelow = true -- 수평 분할시 아래쪽에 창 생성
-
-vim.opt.undofile = true -- 종료시에도 undo 기록을 남김
-
-vim.o.mouse = ""
