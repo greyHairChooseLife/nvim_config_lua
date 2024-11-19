204ab24124c274403d0a9bfa62cbdc28d5e14ac4
sangyeon kim <greyhairchooselife@gmail.com>
2024-11-19 18:29:40 +0900

    etc: 단순 가독성 개선

--- lua/core/keymaps/FT_fugitive.lua
+++ lua/core/keymaps/FT_fugitive.lua
@@ -7,14 +7,17 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.cmd("setlocal winhighlight=Normal:FugitiveBufferHighlight,SignColumn:FugitiveBufferHighlight,EndOfBuffer:FugitiveBufferEOB")
 
 
-    vim.keymap.set('n', 'P', ':G push', { buffer = true })
+    -- Keymap
+    local opts = { buffer = true }
+
+    vim.keymap.set('n', 'P', ':G push', opts)
     vim.keymap.set('n', 'gq', function()
       if vim.fn.winnr('$') == 1 then vim.cmd('q')
       elseif vim.fn.winnr('$') == 2 and require('nvim-tree.api').tree.is_visible() then vim.cmd('q')
       else vim.cmd('bd!') end
-    end, { buffer = true} ) -- close buffer, saving memory
+    end, opts ) -- close buffer, saving memory
     vim.keymap.set('n', 'i', function()
       vim.cmd('normal =')
-    end, { buffer = true }) -- do nothing
+    end, opts) -- do nothing
   end
 })
