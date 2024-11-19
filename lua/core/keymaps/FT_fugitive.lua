9494c184ac126c4bec0434eda1c47505581c0548
sangyeon kim <greyhairchooselife@gmail.com>
2024-11-19 18:31:04 +0900

    keymap: fugitive Fetch with 'F'

--- lua/core/keymaps/FT_fugitive.lua
+++ lua/core/keymaps/FT_fugitive.lua
@@ -11,6 +11,7 @@ vim.api.nvim_create_autocmd("FileType", {
     local opts = { buffer = true }
 
     vim.keymap.set('n', 'P', ':G push', opts)
+    vim.keymap.set('n', 'F', '<Cmd>G fetch<CR>', opts)
     vim.keymap.set('n', 'gq', function()
       if vim.fn.winnr('$') == 1 then vim.cmd('q')
       elseif vim.fn.winnr('$') == 2 and require('nvim-tree.api').tree.is_visible() then vim.cmd('q')
