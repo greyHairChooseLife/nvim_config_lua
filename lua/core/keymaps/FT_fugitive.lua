800ff2ec08d175973cd8cf61c6637a7dce31c66a
sangyeon kim <greyhairchooselife@gmail.com>
2024-12-28 16:28:26 +0900

    fix: fugitive에서 'gq'하면 nvim-tree UI 깨지던 문제

--- lua/core/keymaps/FT_fugitive.lua
+++ lua/core/keymaps/FT_fugitive.lua
@@ -14,13 +14,15 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.keymap.set("n", "P", ":G push", opts)
 		vim.keymap.set("n", "F", "<Cmd>G fetch<CR>", opts)
 		vim.keymap.set("n", "gq", function()
-			if vim.fn.winnr("$") == 1 then
-				vim.cmd("q")
-			elseif vim.fn.winnr("$") == 2 and require("nvim-tree.api").tree.is_visible() then
-				vim.cmd("q")
-			else
-				vim.cmd("bd!")
-			end
+			vim.cmd("q")
+			-- DEPRECATED:: 2024-12-28
+			-- if vim.fn.winnr("$") == 1 then
+			-- 	vim.cmd("q")
+			-- elseif vim.fn.winnr("$") == 2 and require("nvim-tree.api").tree.is_visible() then
+			-- 	vim.cmd("q")
+			-- else
+			-- 	vim.cmd("bd!")
+			-- end
 		end, opts) -- close buffer, saving memory
 		vim.keymap.set("n", "i", function()
 			vim.cmd("normal =")
